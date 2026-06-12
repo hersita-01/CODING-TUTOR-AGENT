4796c029b01cef03c70a7032526feed6ded6072f
hersita-01 <hersitachandak2006@gmail.com>
2026-06-12 13:30:57 +0530

    Final project structure

--- week2-prompt-engineering/day3-socratic/week1-foundations/day2-python-basics/frequency of chars.py
+++
@@ -1,8 +0,0 @@
-#number of times char appears in a str
-string=input("Enter a string:")
-d={}
-for i in string:
-    if i not in d.keys():
-        d[i]=string.count(i)
-print(d)
-        
--- week2-prompt-engineering/day3-socratic/week1-foundations/day3-functions-debugging/random 3 nos.py
+++
@@ -1,9 +0,0 @@
-import random
-def ran(a,b):
-    for i in range(3):
-        x=random.randint(a,b)
-        print("The number is ",x)
-
-d=int(input("Enter "))
-e=int(input("Enter "))
-print(ran(d,e))
